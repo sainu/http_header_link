036afdbf3212c029c99bfeed9e8bd00272cb29c8
sainu <saino.katsutoshi@moneyforward.co.jp>
2020-10-29 19:16:10 +0900

    Require pry in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'http_link_header'
 require 'rspec-parameterized-context'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
